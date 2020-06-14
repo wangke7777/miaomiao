b89b5087d4fa46848645857b5be093cf8046c717
wk <786634240@qq.com>
2020-06-14 11:00:43 +0800

    组件创建

--- src/components/Login/index.vue
+++ src/components/Login/index.vue
@@ -1,5 +1,23 @@
 <template>
-
+    <div class="login_body">
+        <div>
+            <label>
+                <input class="login_text" type="text" placeholder="账户名/手机号/Email" >
+            </label>
+        </div>
+        <div>
+            <label>
+                <input class="login_text" type="password" placeholder="请输入您的密码" >
+            </label>
+        </div>
+        <div class="login_btn">
+            <input type="submit" value="登录">
+        </div>
+        <div class="login_link">
+            <a href="#">立即注册</a>
+            <a href="#">找回密码</a>
+        </div>
+    </div>
 </template>
 
 <script>
