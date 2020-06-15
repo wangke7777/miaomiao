3c73348ea4148ce5eebdab80d202004ce7d079b9
wk <786634240@qq.com>
2020-06-16 00:34:01 +0800

    nowPlaying和comingSoon组件数据渲染

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import store from './store'
 import axios from  'axios'
 Vue.prototype.axios=axios
 Vue.config.productionTip = false
+Vue.filter("setWH",(url,arg)=>{
+    return url.replace(/w\.h/,arg)
+})
 
 new Vue({
     router,
